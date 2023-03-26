6f27eacdcb2b1f25d18c98b2731058144c957b07
Benjaminwills <benjaminwills047@gmail.com>
2023-03-26 20:08:57 +0100

    change time step

--- orbits.py
+++ orbits.py
@@ -8,7 +8,7 @@ traj = Trajectory(
     planets=planets,
     start_time=0,
     end_time=2 * 60 * 60 * 24 * 365,
-    number_of_steps=1e3,
+    number_of_steps=1e6,
 )
 
 trajectory = traj.calculate_trajectories()
